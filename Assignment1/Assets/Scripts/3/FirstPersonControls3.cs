cf3077d296a52604c5bc964cf7fa9e776d91f92c
Nathan McNaughton <namcn@live.com>
2020-02-06 19:54:36 -0600

    Assignment  3

---
+++ Assignment1/Assets/Scripts/3/FirstPersonControls3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonControls3 : MonoBehaviour
+{    
+    float sensitivity = 100f;
+     float xRotation = gameObject.transform.rotation.x;
+     float yRotation = gameObject.transform.rotation.y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
